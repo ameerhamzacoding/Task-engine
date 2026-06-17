14cc2f4e3a8bd8c772242cc9bfde8515f67df624
Ameerweb101 <ameerha1051@gmail.com>
2026-06-17 16:12:01 +0500

    fix: env configuration

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
-const connectDB = () => {
-  const URI = process.env.MONGO_URI;
-  
-  mongoose.connect(URI).then(() => {
-    console.log("MongoDB connection established successfully");
-  }).catch((error) => {
-    console.log("MongoDB connection failed:", error);
-  });
+const connectDB = async () => {
+    // If process.env.MONGO_URI is missing, fall back to trying to load dotenv manually
+    if (!process.env.MONGO_URI) {
+        require('dotenv').config();
+    }
+
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`MongoDB connection failed: ${error.message}`);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
